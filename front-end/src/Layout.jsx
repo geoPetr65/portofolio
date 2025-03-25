88c95b18c4e9e350ec5ead2d439ade36aa3d1eff
Georgios Petrisis <geopetrisis@gmail.com>
2025-03-25 18:33:51 -0400

    URL params with React Router
    finished setting up navigation bar that swaps pages without reloading because it isnt anchored.

---
+++ front-end/src/Layout.jsx
@@ -0,0 +1,15 @@
+import {Outlet} from 'react-router-dom';
+import NavBar from './NavBar';
+
+export default function Layout(){
+   
+    return (
+        <>
+        <NavBar />
+        <Outlet />
+        </>
+        
+    );
+    
+    
+}
